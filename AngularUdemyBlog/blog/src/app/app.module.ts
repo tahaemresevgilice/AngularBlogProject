f88e2ff161c6d797d559739faf2d389aa3498608
Taha Emre Sevgilice <153989336+tahaemresevgilice@users.noreply.github.com>
2024-06-01 02:37:57 +0300

    component

--- AngularUdemyBlog/blog/src/app/app.module.ts
+++ AngularUdemyBlog/blog/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {MatSliderModule} from '@angular/material/slider';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutMeComponent,
+    ContactComponent
   ],
   imports: [
     MatSliderModule,
