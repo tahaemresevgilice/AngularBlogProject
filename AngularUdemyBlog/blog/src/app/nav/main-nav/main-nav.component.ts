adc1a7c2162d9029d9349331e479ba33fbd442b7
Taha Emre Sevgilice <153989336+tahaemresevgilice@users.noreply.github.com>
2024-06-04 18:41:58 +0300

    angular

--- AngularUdemyBlog/blog/src/app/nav/main-nav/main-nav.component.ts
+++ AngularUdemyBlog/blog/src/app/nav/main-nav/main-nav.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+enum MainPage {
+  home = 1,
+  aboutMe = 2,
+  contact = 3,
+}
 
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
-  styleUrl: './main-nav.component.css'
+  styleUrls: ['./main-nav.component.css']
 })
 export class MainNavComponent {
+  pageActive: MainPage = MainPage.home;
 
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.updateActivePage(event.url);
+      }
+    });
+  }
+
+  private updateActivePage(url: string) {
+    switch (url) {
+      case '/anasayfa':
+        this.pageActive = MainPage.home;
+        break;
+      case '/hakkimda':
+        this.pageActive = MainPage.aboutMe;
+        break;
+      case '/iletisim':
+        this.pageActive = MainPage.contact;
+        break;
+      default:
+        this.pageActive = MainPage.home;
+    }
+  }
+
+  ngOnInit() {}
 }
