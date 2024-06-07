b126951ed31d1f5058f27e5da2c7f058fc798242
Taha Emre Sevgilice <153989336+tahaemresevgilice@users.noreply.github.com>
2024-06-08 00:06:16 +0300

    angular

--- AngularUdemyBlog/blog/src/app/services/category.service.ts
+++ AngularUdemyBlog/blog/src/app/services/category.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
+import { Category } from '../models/category';
 
 @Injectable({
   providedIn: 'root'
